994e16f577333560042927ff6724a9769c16a4ac
masoud <mosibatzadeh@yahoo.com>
2017-09-30 13:50:59 +0330

    heartbeat added

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
 const classes = require('./lib/kcn')
 
-let client = new classes.KCN('ws://localhost/kurento')
+let client = new classes.KCN('ws://localhost:8888/kurento')
 
 client.heartBeat((err, res) => {
   console.log(res === 'ok' ? `kms ❤️ ${client.upTime / 1000} sec` : `kms 💔 after ${client.upTime / 1000}`)
 })
 
-// disableHeartBeat ()
+// async function ping () {
+//   let response = await client.ping()
+//   console.log(response)
+// }
+
+// ping().then(() => { console.log('ping done!') })
+
+async function sampleWiring () {
+  let pipeline = await client.createPipeline() // backbone
+  console.log(pipeline)
+  let webRTCEndpoint = await client.createWebRTCEndpoint(pipeline) // add user
+  console.log(webRTCEndpoint)
+  // add ice candidate of user to WebRTCEndpoint
+  let res = await client.releaseElement(pipeline) // release resources
+  pipeline.dispose()
+  return res
+}
+
+sampleWiring().then(() => {
+  console.log('Wiring done!')
+})
